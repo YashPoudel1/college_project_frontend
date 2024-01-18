c8b675a25d5d0db58d7d883729c67e804e47d6d9
Yashish <yashpoudel3@gmail.com>
2024-01-18 09:01:20 +0545

    feat: auth

---
+++ src/Pages/Signin.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react'
+import { useHistory  } from "react-router-dom";
+import './Signin.css'
+import axios from 'axios'
+import { AuthContext } from '../Context/AuthContext.js'
+import Switch from '@material-ui/core/Switch';
+
+function Signin() {
+    const [isStudent, setIsStudent] = useState(true)
+    const [admissionId, setAdmissionId] = useState()
+    const [employeeId,setEmployeeId] = useState()
+    const [password, setPassword] = useState()
+    const [error, setError] = useState("")
+    const { dispatch } = useContext(AuthContext)
+    const history = useHistory ()
+
+    const API_URL = "http://localhost:5000/"
+
+    console.log("API_URL",API_URL)
+    
+    const loginCall = async (e) => {
+        e.preventDefault()
+        // console.log("userCredential",userCredential)
+        const postData = {
+            username : admissionId,
+            password : password,
+        }
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post(`${API_URL}api/auth/signin`, postData);
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+            history.push("/dashboard@admin")
+        }
+        catch (err) {
+            dispatch({ type: "LOGIN_FAILURE", payload: err })
+            setError("Wrong Password Or Username")
+        }
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault()
+        isStudent
+        ? loginCall({ admissionId, password }, dispatch)
+        : loginCall({ employeeId,password }, dispatch)
+    }
+
+    return (
+        <div className='signin-container'>
+            
+            <div className="signin-card">
+                <form onSubmit={(e)=>{loginCall(e)}}>
+                    <h2 className="signin-title"> Log in</h2>
+                    <p className="line"></p>
+                    <div className="persontype-question">
+                        <p>Are you a Staff member ?</p>
+                        <Switch
+                            onChange={() => setIsStudent(!isStudent)}
+                            color="primary"
+                        />
+                    </div>
+                    <div className="error-message"><p>{error}</p></div>
+                    <div className="signin-fields">
+                        <label htmlFor={isStudent?"admissionId":"employeeId"}> <b>{isStudent?"Admission ID":"Employee ID"}</b></label>
+                        <input className='signin-textbox' type="text" placeholder={isStudent?"Enter Admission ID":"Enter Employee ID"} name={isStudent?"admissionId":"employeeId"} required onChange={(e) => { isStudent?setAdmissionId(e.target.value):setEmployeeId(e.target.value) }}/>
+                        <label htmlFor="password"><b>Password</b></label>
+                        <input className='signin-textbox' type="password" minLength='6' placeholder="Enter Password" name="psw" required onChange={(e) => { setPassword(e.target.value) }} />
+                        </div>
+                    <button type='submit' className="signin-button">Log In</button>
+                    <a className="forget-pass" href="#home">Forgot password?</a>
+                </form>
+                <div className='signup-option'>
+                    <p className="signup-question">Don't have an account? Contact Librarian</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signin
